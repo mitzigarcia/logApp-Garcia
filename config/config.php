7abab66b4bc64f9dc4db65c51fd7558c4fd6cd3b
Mitzi Almira Garcia <54237104+mitzigarcia@users.noreply.github.com>
2021-05-28 16:20:57 +0800

    first commit

---
+++ config/config.php
@@ -0,0 +1,5 @@
+<?php
+	define('DB_HOST', 'sql6.freesqldatabase.com:3306');
+	define('DB_USER', 'sql6415447');
+	define('DB_PASS', '5nAMl7M9XE');
+	define('DB_NAME', 'sql6415447');
---
+++ inc/footer.php
@@ -0,0 +1,7 @@
+<footer class="mastfoot mt-auto" style="text-align: center;">
+    <div class="inner">
+      <p>Created by <a href="https://github.com/hmcldryl">hmcldryl</a> for CC 6/L - IT3B1</p>
+    </div>
+</footer>
+  </body>
+</html>
